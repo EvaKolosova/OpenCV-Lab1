cd2ad8f63d198526e2f0b2ac6f6dda5619664ccf
ekolosova <janejordger@gmail.com>
2019-11-21 18:58:17 +0300

    middle status cause i am not sure about 7 step - it is working strange

--- OpenCV.py
+++ OpenCV.py
@@ -1,48 +1,11 @@
+import time
+
 import cv2 as cv
 import numpy as np
 from matplotlib import pyplot as plt
 
-## --- task1 - оригинальное изображение
-imageSource = '/home/ekolosova/Desktop/bear.jpeg'
-image = cv.imread(imageSource)
-gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
 
-if image is not None:
-    cv.imshow('Original image', image) ## вывод исходного изображения
-
-
-    ## --- task2 - полутоновое изображение
-    cv.imshow('Gray sample', gray)  ## вывод полутонового изображения
-elif image is None:
-    print("Error loading image")
-
-
-## --- task3 - улучшенная контрастность
-clahe = cv.createCLAHE(clipLimit=3., tileGridSize=(8, 8)) # CLAHE (Contrast Limited Adaptive Histogram Equalization)
-
-lab = cv.cvtColor(image, cv.COLOR_BGR2LAB)  # convert from BGR to LAB color space
-l, a, b = cv.split(lab)  # split on 3 different channels
-
-l2 = clahe.apply(l)  # apply CLAHE to the L-channel
-
-lab = cv.merge((l2, a, b))  # merge channels
-image2 = cv.cvtColor(lab, cv.COLOR_LAB2BGR)  # convert from LAB to BGR
-cv.imshow('Increased contrast', image2) ## изображение с улучшеным контрастом
-
-
-## --- task4 - Canny - края обьектов
-edges = cv.Canny(image, 80, 150)
-cv.imshow('Edges from Canny', edges) ## изображение с Canny - края обьектов
-
-
-## --- task5 - угловые точки обьектов, нарисовать кругом с радиусом=2 в изображение с краями
-source_window = 'Source image'
-corners_window = 'Corners detected'
-max_thresh = 255
-
-def cornerHarris_demo(val):
-    thresh = val
-
+def cornerHarris_demo(thresh):
     # Detector parameters
     blockSize = 2
     apertureSize = 3
@@ -55,88 +18,129 @@ def cornerHarris_demo(val):
     dst_norm = np.empty(dst.shape, dtype=np.float32)
     cv.normalize(dst, dst_norm, alpha=0, beta=255, norm_type=cv.NORM_MINMAX)
     dst_norm_scaled = cv.convertScaleAbs(dst_norm)
+    print("corners", dst_norm_scaled)
 
     # Drawing a circle around corners
     for i in range(dst_norm.shape[0]):
         for j in range(dst_norm.shape[1]):
-            if int(dst_norm[i,j]) > thresh:
-                cv.circle(dst_norm_scaled, (j,i), 2, (0), 2) #радиус=2
+            if int(dst_norm[i, j]) > thresh:
+                cv.circle(dst_norm_scaled, (j, i), 2, 0, 2)  # радиус=2
 
     # Showing the result
     cv.namedWindow(corners_window)
     cv.imshow(corners_window, dst_norm_scaled)
 
+
+def limit(value, min, max):
+    if value < min:
+        return min
+    if value > max:
+        return max
+    return value
+
+
+def averageIntegral(data, dist, clear_image, k=150):
+    res_image = data.copy()
+
+    for x in range(data.shape[0]):
+        for y in range(data.shape[1]):
+
+            step = int(k * dist[x][y] / 2)
+
+            x_new = limit(x + step + 1, 0, data.shape[0] - 1)
+            y_new = limit(y + step + 1, 0, data.shape[1] - 1)
+            x_old = limit(x - step, 0, data.shape[0] - 1)
+            y_old = limit(y - step, 0, data.shape[0] - 1)
+
+            integral_formula = clear_image[x_new, y_new] + clear_image[x_old, y_old] - clear_image[x_new, y_old] - \
+                             clear_image[x_old, y_new]  # формула точек в интегральном изображении
+
+            if(step != 0):
+                integral_formula /= (x_new - x_old) * (y_new - y_old)
+                res_image[x][y] = integral_formula
+
+    return res_image
+
+
+def average(data, dist, k=150):
+    res_image = data.copy()
+
+    for x in range(data.shape[0]):
+        for y in range(data.shape[1]):
+
+            step = int(k * dist[x][y] / 2)
+
+            if(step != 0):
+                res_image[x][y] = cv.blur(res_image[x][y], step)
+
+    return res_image
+
+
+## --- task1 - оригинальное изображение
+imageSource = '/home/ekolosova/Desktop/bear.jpg'
+image = cv.imread(imageSource)
+
+if image is not None:
+    cv.imshow('Original image', image)  ## вывод исходного изображения
+
+    ## --- task2 - полутоновое изображение
+    gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+    cv.imshow('Gray sample', gray)  ## вывод полутонового изображения
+elif image is None:
+    print("Error loading image")
+
+## --- task3 - улучшенная контрастность
+clahe = cv.createCLAHE(clipLimit=3., tileGridSize=(8, 8))  # CLAHE (Contrast Limited Adaptive Histogram Equalization)
+
+lab = cv.cvtColor(image, cv.COLOR_BGR2LAB)  # convert from BGR to LAB color space
+l, a, b = cv.split(lab)  # split on 3 different channels
+
+l2 = clahe.apply(l)  # apply CLAHE to the L-channel
+
+lab = cv.merge((l2, a, b))  # merge channels
+image2 = cv.cvtColor(lab, cv.COLOR_LAB2BGR)  # convert from LAB to BGR
+cv.imshow('Increased contrast', image2)  ## изображение с улучшеным контрастом
+
+## --- task4 - Canny - края обьектов
+edges = cv.Canny(image, 80, 150)
+print("edges", edges)
+cv.imshow('Edges from Canny', edges)  ## изображение с Canny - края обьектов
+edges_temp = cv.bitwise_not(edges)
+
+## --- task5 - угловые точки обьектов, нарисовать кругом с радиусом=2 в изображение с краями
+source_window = 'Source image'
+corners_window = 'Corners detected'
+max_thresh = 255
+
 # Create a window and a trackbar
 cv.namedWindow(source_window)
-thresh = 200 # initial threshold
+thresh = 200  # initial threshold
 cv.createTrackbar('Threshold: ', source_window, thresh, max_thresh, cornerHarris_demo)
 cv.imshow(source_window, image)
 cornerHarris_demo(thresh)
 
-
 ## --- task6 - для границ и угловых точек построить карту расстояний
-data = np.array(gray)
-ret, binary = cv.threshold(data, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU) #пороговое=бинарное изображение
-cv.imshow('Binary image', binary)
+# data = np.array(gray)
+# ret, binary = cv.threshold(data, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU) #пороговое=бинарное изображение
+# cv.imshow('Binary image', binary)
 
-# Perform the distance transform algorithm
-dist = cv.distanceTransform(binary, cv.DIST_L2, 3)
-# Normalize the distance image for range = {0.0, 1.0}
-# so we can visualize and threshold it
-cv.normalize(dist, dist, 0, 1.0, cv.NORM_MINMAX)
-cv.imshow('Distance Transform Image', dist)
+dist = cv.distanceTransform(edges_temp, cv.DIST_L2, 3)
+dist_not_norm = dist
+print("dist_transform", dist_not_norm)
+cv.normalize(dist_not_norm, dist_not_norm, 0, 1.0, cv.NORM_MINMAX)
+cv.imshow('Distance Transform Image', dist_not_norm)
 
 ## --- task7 - в каждом пикселе фильтрация усреднением
-# kernel = np.ones((5, 5), np.float32)/25
-# dst = cv.filter2D(image, -1, kernel) # or use # blur = cv.blur(image, (5, 5)), но результат идентичный
-# plt.subplot(121), plt.imshow(image), plt.title('Original')
-# plt.xticks([]), plt.yticks([])
-# plt.subplot(122), plt.imshow(dst), plt.title('Averaging')
-# plt.xticks([]), plt.yticks([])
-# plt.show() #для визуализации и удобного сравнения
+blur_image = average(gray, dist)
 
-# blur = cv.blur(image, (5, 5))
-# cv.imshow("blur", blur)
-
-
-def dist_mask(mask, max_dist=10):
-    mask = mask.astype(np.uint8)
-
-    def get_dist(m):
-        d = cv.distanceTransform(m, cv.DIST_L2, maskSize=3)
-        d[d > max_dist] = max_dist
-        return d / max_dist
-
-    dist = get_dist(mask) - get_dist(1 - mask)
-
-    return (1 + dist) / 2
-
-def smoothBy_Averaging(image, kernelX, kernelY):
-    """https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_imgproc/py_filtering/py_filtering.html """
-    blur = cv.blur(image, (int(kernelX), int(kernelY)))
-    return blur
-
-height = np.size(image, 0)
-width = np.size(image, 1)
-
-for y in range(0, height-1):
-        for x in range(0, width-1):
-            #pixel = image[i, j]
-            imageBlur = smoothBy_Averaging(image, dist, dist)
-
-cv.imshow("blur", imageBlur)
-
-
+cv.imshow("Blur", cv.UMat(blur_image))
 
 ## --- task8 - интегральные изображения в фильтрации усреднением
+integral_image = cv.integral(gray)
+blur_image_int = averageIntegral(gray, dist, integral_image)
 
+cv.imshow("Blur with integral image", cv.UMat(blur_image_int))
 
 
 k = cv.waitKey(0)
 cv.destroyAllWindows()
-
-
-
-
-
-
